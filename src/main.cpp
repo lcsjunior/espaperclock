d29aa82917956d532187b34695ed6e71f095c4cd
lcsjunior <lcsjuniorsi@gmail.com>
2026-07-28 23:08:08 -0300

    feat(led): remove builtin LED status blink
    Panel bring-up no longer needs a boot indicator; drop StatusLed and
    its GPIO22 wiring instead of carrying dead peripheral code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <BuiltinLed.h>
 #include <Config.h>
 #include <OpenWeatherMap.h>
 
@@ -7,12 +6,8 @@
 #include "modules/wifi_setup.h"
 
 #define SERIAL_BAUD_RATE 115200
-#define STATUS_LED_PIN 22
 
 void setup() {
-  StatusLed.begin(STATUS_LED_PIN);
-  StatusLed.blink();
-
   Serial.begin(SERIAL_BAUD_RATE);
 
   AppConfig.mount();
